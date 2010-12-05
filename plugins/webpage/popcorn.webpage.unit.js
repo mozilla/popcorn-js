ca33309e21d8a63c69108bfcebcc7fad743f880e
Anna Sobiepanek <annaopensource@gmail.com>
2010-12-05 16:12:17 -0500

    trying out tests

--- plugins/webpage/popcorn.webpage.unit.js
+++ plugins/webpage/popcorn.webpage.unit.js
@@ -1,21 +1,38 @@
-$(function(){
+test("Popcorn Webpage Plugin", function () {
+  
+  
+  // needs expectation
 
-  test("Popcorn Webpage Plugin", function () {
+  var popped = Popcorn("#video"),
+      expects = 4,
+      count = 0;
+      
+  expect(expects);
+      
+  function plus (){
+    if(++count === expects){
+      stop();
+    }
+  }
+  
+  popped.webpages({
+      id: "webpages-a", 
+      start: 0, // seconds
+      end: 5, // seconds
+      src: 'http://www.webmademovies.org',
+      target: 'webpagediv'
+    });
     
-    
-    // needs expectation
-
-    var popped = Popcorn("#video");
-     
-    popped.webpages();
-    
-    
-    ok( 'data' in popped, "popped has the data prop and therefore is a Popcorn object" )
-    
-    ok( 'webpages' in popped, "webpages is a mehtod of the popped instance");
-    
-    ok( typeof popped.webpages === "function" , "webpages is in the plugins registry");
-
-    
-  });
+  popped.play();
+  
+  
+  ok( 'data' in popped, "popped has the data prop and therefore is a Popcorn object" )
+  
+  ok( 'webpages' in popped, "webpages is a mehtod of the popped instance");
+  
+  ok( typeof popped.webpages === "function" , "webpages is a function");
+  
+  popped.exec(2, function(){
+    ok( document.getElementById('webpages-a') , "iframe exists");  
+  })
 });
