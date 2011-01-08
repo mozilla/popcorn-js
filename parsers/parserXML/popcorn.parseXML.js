3cdf6a0e2e2a8a0443ec244ef89aeed32db4e686
ScottDowne <sdowne@learn.senecac.on.ca>
2011-01-08 13:16:28 -0500

    [#246] initial conversion of 0.1 semantic demo

--- parsers/parserXML/popcorn.parseXML.js
+++ parsers/parserXML/popcorn.parseXML.js
@@ -16,6 +16,21 @@
         },
         manifestData = {};
 
+    // Simple function to convert 0:05 to 0.5 in seconds
+    // acceptable formats are HH:MM:SS:MM, MM:SS:MM, SS:MM, SS
+    var toSeconds = function(time) {
+      var t = time.split(":");
+      if (t.length === 1) {
+        return parseFloat(t[0], 10);
+      } else if (t.length === 2) {
+        return parseFloat(t[0], 10) + parseFloat(t[1] / 12, 10);
+      } else if (t.length === 3) {
+        return parseInt(t[0] * 60, 10) + parseFloat(t[1], 10) + parseFloat(t[2] / 12, 10);
+      } else if (t.length === 4) {
+        return parseInt(t[0] * 3600, 10) + parseInt(t[1] * 60, 10) + parseFloat(t[2], 10) + parseFloat(t[3] / 12, 10);
+      }
+    };
+
     // turns a node tree element into a straight up javascript object
     // also converts in and out to start and end
     // also links manifest data with ids
@@ -30,10 +45,10 @@
 
         // converts in into start
         if (key === "in") {
-          returnObject.start = data;
+          returnObject.start = toSeconds( data );
         // converts out into end
         } else if ( key === "out" ){
-          returnObject.end = data;
+          returnObject.end = toSeconds( data );
         // this is where ids in the manifest are linked
         } else if ( key === "resourceid" ) {
           Popcorn.extend( returnObject, manifestData[data] );
