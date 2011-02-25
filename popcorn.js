6847060805e4f43d54610c51386587c2f758d072
ScottDowne <sdowne@learn.senecac.on.ca>
2011-02-25 11:48:49 -0500

    [#369] create empty start and end function

--- popcorn.js
+++ popcorn.js
@@ -484,6 +484,9 @@
 
       //  Push track event ids into the history
       obj.data.history.push( track._id );
+
+      track._natives.start = track._natives.start || Popcorn.nop;
+      track._natives.end = track._natives.end || Popcorn.nop;
     }
 
     // Store this definition in an array sorted by times
