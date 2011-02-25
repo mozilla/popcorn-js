060d31d5f51750968fd40caf8a6ba1526adda3df
Anna Sobiepanek <annaopensource@gmail.com>
2011-02-25 12:50:30 -0500

    fixed src in order to pass unit tests [#358 state:peer-review-requested]

--- plugins/wikipedia/popcorn.wikipedia.js
+++ plugins/wikipedia/popcorn.wikipedia.js
@@ -89,7 +89,7 @@ var wikiCallback;
       
       var head   = document.getElementsByTagName( "head" )[0];
       var script = document.createElement( "script" );
-      script.src = "http://" + options.lang + ".wikipedia.org/w/api.php?action=parse&props=text&page=" + ( options.title || options.src.slice( options.src.lastIndexOf("/")+1) ) + "&format=json&callback=wikiCallback" + _guid;
+      script.src = "http://" + options.lang + ".wikipedia.org/w/api.php?action=parse&props=text&page=" + ( options.src.slice( options.src.lastIndexOf("/")+1) ) + "&format=json&callback=wikiCallback" + _guid;
 
       head.insertBefore( script, head.firstChild );        
     },
