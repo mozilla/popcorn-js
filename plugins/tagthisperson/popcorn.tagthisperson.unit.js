e29f31684fb2f0359a7dd5f34d23dc8a3e801e52
Minoo <minoo.ziaei@gmail.com>
2011-03-02 21:23:44 -0500

    Recreated tagthispersonNew branch based on the latest tagthisperson plugin code -#91

--- plugins/tagthisperson/popcorn.tagthisperson.unit.js
+++ plugins/tagthisperson/popcorn.tagthisperson.unit.js
@@ -1,17 +1,19 @@
 test("Popcorn tagthisperson Plugin", function () {
   
   var popped = Popcorn( "#video" ),
-      expects = 7, 
+      expects = 8, 
       count = 0,
       interval,
       interval2,
       interval3,
+      interval4,
       tagdiv = document.getElementById( 'tagdiv' );
+      tagdiv2 = document.getElementById( 'tagdiv2' );
   
   expect(expects);
   
   function plus() {
-    if ( ++count===expects ) {
+    if ( ++count === expects ) {
       start();
     }
   }
@@ -29,21 +31,35 @@ test("Popcorn tagthisperson Plugin", function () {
       end: 5, // seconds
       person: 'Anna Sob',
       image: 'http://newshour.s3.amazonaws.com/photos%2Fspeeches%2Fguests%2FRichardNSmith_thumbnail.jpg',
+      href: 'http://annasob.wordpress.com',           
       target: 'tagdiv'
     } )
     .tagthisperson({
       start: 3, // seconds
       end: 10, // seconds
       person: 'Scott',
+      href: 'http://scottdowne.wordpress.com/',
       target: 'tagdiv'
     } )
+    .tagthisperson({
+      start: 6, // seconds
+      end: 10, // seconds
+      person: 'Mike',
+      target: 'tagdiv2'
+    } )    
+    .tagthisperson({
+      start: 7, // seconds
+      end: 12, // seconds
+      person: 'Daniel',
+      target: 'tagdiv2'
+    } )     
     .volume(0)
     .play();
   
   
   interval = setInterval( function() {
     if( popped.currentTime() > 0 && popped.currentTime() <= 5 ) {
-      equals ( tagdiv.childElementCount, 1, "tagdiv now contains one child elements" );
+      equals ( tagdiv.childElementCount, 2, "tagdiv now contains two child elements" );
       plus();
       equals ( tagdiv.textContent.trim() , "Anna Sob" ,"tagdiv shows the first tag" );
       plus();
@@ -52,25 +68,23 @@ test("Popcorn tagthisperson Plugin", function () {
   }, 2000);
   
   interval2 = setInterval( function() {
-    if( popped.currentTime() > 3 && popped.currentTime() < 5  ) {
-      equals ( tagdiv.textContent.trim() , "Anna Sob, Scott", "tagdiv shows the first & second tag" );
+    if( popped.currentTime() > 5  ) {
+      equals ( tagdiv.innerHTML.search( "<a href" ) , 1 ,"second tag in tagdiv has a url" );
+      plus();
+      equals ( tagdiv2.textContent.trim(), "Mike, Daniel" ,"tagdiv2 shows the first & second tag" );
       plus();
       clearInterval( interval2 );
     }
-  }, 2000);
+  }, 4000);
   
   interval3 = setInterval( function() {
-    if( popped.currentTime() > 5 ) {
-      equals ( tagdiv.innerHTML.trim() , "Scott" ,"tagdiv shows the second tag only" );
+    if( popped.currentTime() > 12 ) {
+      equals ( tagdiv.innerHTML , "" ,"tagdiv is now cleared" );
+      plus();
+      equals ( tagdiv2.innerHTML , "" ,"tagdiv2 is now cleared" );
       plus();
       clearInterval( interval3 );
     }
-  }, 5000);
-  interval4 = setInterval( function() {
-    if( popped.currentTime() > 10 ) {
-      equals ( tagdiv.innerHTML , "" ,"tagdiv is now cleared" );
-      plus();
-      clearInterval( interval4 );
-    }
   }, 4000);
+
 });
