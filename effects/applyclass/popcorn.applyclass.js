7e01635dd1b1b386e82fbfb45521e0613569f2a0
ScottDowne <sdowne@learn.senecac.on.ca>
2011-06-21 19:08:16 -0400

    [#t366] created applyclass effect, appied applyclass to semantic demo

---
+++ effects/applyclass/popcorn.applyclass.js
@@ -0,0 +1,88 @@
+// EFFECT: applyclass
+
+(function (Popcorn) {
+
+  /**
+   * apply css class to jquery selector
+   * selector is relative to plugin target's id
+   * so .overlay is actually $( "#target .overlay")
+   *
+   * @param {Object} options
+   * 
+   * Example:
+     var p = Popcorn('#video')
+        .footnote({
+          start: 5, // seconds
+          end: 15, // seconds
+          text: 'This video made exclusively for drumbeat.org',
+          target: 'footnotediv',
+          effect: 'applyclass',
+          applyclass: 'selector: class'
+        })
+   *
+   */
+
+  var startFunction = Popcorn.nop,
+      applyFunction = function() {
+
+        startFunction = function( event, options ) {
+
+          Popcorn.forEach( options.classes, function( val, key ) {
+
+            var obj;
+            if ( key === "parent" ) {
+
+              obj = $("#" + options.target ).parent();
+            } else {
+
+              obj = $("#" + options.target + " " + key );
+            }
+
+            obj.addClass( val );
+            setTimeout( function() {
+
+              obj.removeClass( val );
+            }, 250 );
+          });
+        };
+      };
+
+  if ( typeof $ !== "undefined" ) {
+
+    applyFunction();
+  } else {
+
+    Popcorn.getScript( "http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", applyFunction );
+  }
+
+  Popcorn.compose( "applyclass", {
+    
+    manifest: {
+      about: {
+        name: "Popcorn applyclass Effect",
+        version: "0.1",
+        author: "@scottdowne",
+        website: "scottdowne.wordpress.com"
+      },
+      options: {}
+    },
+    _setup: function( options ) {
+
+      options.classes = {};
+
+      var array = options.applyclass.replace( /\s/g, "" ).split( "," ),
+          item = [],
+          idx = 0, len = array.length;
+
+      for ( ; idx < len; idx++ ) {
+
+        item = array[ idx ].split( ":");
+        options.classes[ item[ 0 ] ] = item[ 1 ];
+      }
+    },
+    start: function( event, options ) {
+
+      startFunction.call( this, event, options );
+    }
+  });
+})( Popcorn );
