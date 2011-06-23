5e2e71702d5ee7be52464ab30124a70b553bb924
David Seifried <david.c.seifried@gmail.com>
2011-06-23 14:42:35 -0400

    [#578] Moved a + sign to fix linting error

--- plugins/wikipedia/popcorn.wikipedia.js
+++ plugins/wikipedia/popcorn.wikipedia.js
@@ -119,8 +119,8 @@ var wikiCallback;
       };
       
       if ( options.src ) {
-        Popcorn.getScript( "http://" + options.lang + ".wikipedia.org/w/api.php?action=parse&props=text&page=" 
-          + options.src.slice( options.src.lastIndexOf("/")+1)  + "&format=json&callback=wikiCallback" + _guid);
+        Popcorn.getScript( "http://" + options.lang + ".wikipedia.org/w/api.php?action=parse&props=text&page=" + 
+          options.src.slice( options.src.lastIndexOf("/")+1)  + "&format=json&callback=wikiCallback" + _guid);
       } else {
         throw ( "Wikipedia plugin needs a 'src'" );
       }
