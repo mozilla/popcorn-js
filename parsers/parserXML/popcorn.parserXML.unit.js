35aaae71990d8ff1c4dd7440631013bbeb109879
ScottDowne <sdowne@learn.senecac.on.ca>
2011-06-24 17:22:29 -0400

    [#619] clean up XML parser unit test

--- parsers/parserXML/popcorn.parserXML.unit.js
+++ parsers/parserXML/popcorn.parserXML.unit.js
@@ -53,11 +53,8 @@ test("Popcorn 0.1 XML Parser Plugin", function () {
     end: function ( event, options ) {}
   });
 
-  poppercorn.parseXML("data/unit.XML");
-
-  // interval used to wait for data to be parsed
-  interval = setInterval( function() {
-    poppercorn.currentTime(5).play().currentTime(6);
-  }, 2000);
-  
+  poppercorn.parseXML("data/unit.XML", function() {
+    poppercorn.currentTime(5);
+  });  
 });
+
