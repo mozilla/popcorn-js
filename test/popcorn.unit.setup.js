f8838ffe6659695aaf15dab00d7a50fc9e023804
Rick Waldron <waldron.rick@gmail.com>
2011-09-06 16:26:48 -0400

    [#700] Removes unnec. whitespace

--- test/popcorn.unit.setup.js
+++ test/popcorn.unit.setup.js
@@ -47,15 +47,11 @@ if ( !window["console"] ) {
     return size;
   };
 
-
-
   Setup.eventset  = "loadstart progress suspend emptied stalled play pause " +
                           "loadedmetadata loadeddata waiting playing canplay canplaythrough " +
                           "seeking seeked timeupdate ended ratechange durationchange volumechange";
 
   Setup.events = Setup.eventset.split(/\s+/g);
-
-
 
   global.Setup = Setup;
 
