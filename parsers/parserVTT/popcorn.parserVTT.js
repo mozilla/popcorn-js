656d5922ddd556d949cd00fb97585a8df8e6e514
stevenaw <sweerdenburg@gmail.com>
2011-10-12 20:39:58 -0400

    [#750] Fix whitespace, quotes issues

--- parsers/parserVTT/popcorn.parserVTT.js
+++ parsers/parserVTT/popcorn.parserVTT.js
@@ -16,15 +16,15 @@
     00:32.500 --> 00:00:33.500 A:start S:50% D:vertical L:98%
     <v Neil DeGrass Tyson><i>Laughs</i>
    */  
-  Popcorn.parser( 'parseVTT', function( data ) {
+  Popcorn.parser( "parseVTT", function( data ) {
   
     // declare needed variables
     var retObj = {
-          title: '',
-          remote: '',
-          data: []
+          title: "",
+          remote: "",
+          data: [  ]
         },
-        subs = [],        
+        subs = [  ],        
         i = 0,
         len = 0,
         lines,
@@ -38,27 +38,27 @@
     len = lines.length;
     
     // Check for BOF token
-    if ( len === 0 || lines[0] !== 'WEBVTT' ) {
+    if ( len === 0 || lines[ 0 ] !== "WEBVTT" ) {
       return retObj;
     }
     
     i++;
     
     while ( i < len ) {
-      text = [];
+      text = [  ];
       
       try {
         i = skipWhitespace( lines, len, i );
-        sub = parseCueHeader( lines[i++] );
+        sub = parseCueHeader( lines[ i++ ] );
         
         // Build single line of text from multi-line subtitle in file
-        while ( i < len && lines[i] ) {
-          text.push( lines[i++] );
+        while ( i < len && lines[ i ] ) {
+          text.push( lines[ i++ ] );
         }
         
         // Join lines together to one and build subtitle text
-        sub.text = text.join( '<br />' );        
-        subs.push( createTrack( 'subtitle', sub ) );
+        sub.text = text.join( "<br />" );        
+        subs.push( createTrack( "subtitle", sub ) );
       } catch ( e ) {
         i = skipNonWhitespace( lines, len, i );
       }
@@ -71,26 +71,26 @@
   // [HH:]MM:SS.mmm string to SS.mmm float
   // Throws exception if invalid
   function toSeconds ( t_in ) {
-    var t = t_in.split( ':' ),
+    var t = t_in.split( ":" ),
         l = t_in.length,
         time;
     
     // Invalid time string provided
     if ( l !== 12 && l !== 9 ) {
-      throw 'Bad cue';
+      throw "Bad cue";
     }
     
     l = t.length - 1;
     
     try {        
-      time = parseInt( t[l-1], 10 ) * 60 + parseFloat( t[l], 10 );
+      time = parseInt( t[ l-1 ], 10 ) * 60 + parseFloat( t[ l ], 10 );
       
       // Hours were given
       if ( l === 2 ) {
-        time += parseInt( t[0], 10 ) * 3600;
+        time += parseInt( t[ 0 ], 10 ) * 3600;
       }
     } catch ( e ) {
-      throw 'Bad cue';
+      throw "Bad cue";
     }
     
     return time;
@@ -98,7 +98,7 @@
   
   function createTrack( name, attributes ) {
     var track = {};
-    track[name] = attributes;
+    track[ name ] = attributes;
     return track;
   }
   
@@ -109,25 +109,25 @@
         rToken = /-->/,
         rWhitespace = /[\t ]+/;
       
-    if ( !line || line.indexOf( '-->' ) === -1 ) {
-      throw 'Bad cue';
+    if ( !line || line.indexOf( "-->" ) === -1 ) {
+      throw "Bad cue";
     }
   
-    lineSegments = line.replace( rToken, ' --> ' ).split( rWhitespace );
+    lineSegments = line.replace( rToken, " --> " ).split( rWhitespace );
     
     if ( lineSegments.length < 2 ) {
-      throw 'Bad cue';
+      throw "Bad cue";
     }
     
     sub.id = line;
-    sub.start = toSeconds( lineSegments[0] );
-    sub.end = toSeconds( lineSegments[2] );
+    sub.start = toSeconds( lineSegments[ 0 ] );
+    sub.end = toSeconds( lineSegments[ 2 ] );
     
     return sub;
   }
   
   function skipWhitespace ( lines, len, i ) {
-    while ( i < len && !lines[i] ) {
+    while ( i < len && !lines[ i ] ) {
       i++;
     }
     
@@ -135,7 +135,7 @@
   }
   
   function skipNonWhitespace ( lines, len, i ) {
-    while ( i < len && lines[i] ) {
+    while ( i < len && lines[ i ] ) {
       i++;
     }
     
