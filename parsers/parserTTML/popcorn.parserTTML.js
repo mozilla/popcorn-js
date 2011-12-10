f5b8c1176f66a28b911f04989da7931f8d265f6e
stevenaw <sweerdenburg@gmail.com>
2011-12-10 18:15:40 -0500

    [#752] Optimizations, whitespace cleanup, demo update

--- parsers/parserTTML/popcorn.parserTTML.js
+++ parsers/parserTTML/popcorn.parserTTML.js
@@ -34,13 +34,12 @@
           remote: "",
           data: []
         },
-        node,
-        numTracks = 0,
-        region;
+        node;
 
     // Null checks
-    if ( !data.xml || !data.xml.documentElement )
+    if ( !data.xml || !data.xml.documentElement ) {
       return returnData;
+    }
 
     node = data.xml.documentElement.firstChild;
 
@@ -54,25 +53,22 @@
     }
 
     if ( node ) {
-      region = node.getAttribute( "region" );
       parseChildren( node, 0 );
     }
 
     return returnData;
 
     // Parse the children of the given node
-    function parseChildren( node, timeOffset ) {
+    function parseChildren( node, timeOffset, region ) {
       var currNode = node.firstChild,
-          sub,
+          currRegion = getNodeRegion( node, region ),
           newOffset;
 
       while ( currNode ) {
         if ( currNode.nodeType === 1 ) {
           if ( currNode.nodeName === "p" ) {
             // p is a textual node, process contents as subtitle
-            sub = parseNode( currNode, timeOffset );
-            returnData.data.push( createTrack( "subtitle", sub ) );
-            numTracks++;
+            returnData.data.push( parseNode( currNode, timeOffset, currRegion ) );
           } else if ( currNode.nodeName === "div" ) {
             // div is container for subtitles, recurse
             newOffset = toSeconds( currNode.getAttribute( "begin" ) );
@@ -81,41 +77,53 @@
               newOffset = timeOffset;
             }
 
-            parseChildren( currNode, newOffset );
+            parseChildren( currNode, newOffset, currRegion );
           }
         }
 
         currNode = currNode.nextSibling;
       }
     }
-
-    // Parse a node for text content
-    function parseNode( node, timeOffset ) {
-      var sub = {};
-
-      // Trim left and right whitespace from text and convert non-explicit line breaks
-      sub.text = node.textContent.replace( rWhitespace, "" ).replace( rLineBreak, "<br />" );
-      sub.id = node.getAttribute( "xml:id" ) || node.getAttribute( "id" );
-      sub.start = toSeconds ( node.getAttribute( "begin" ), timeOffset );
-      sub.end = toSeconds( node.getAttribute( "end" ), timeOffset );
-      sub.target = region;
-
-      if ( sub.end < 0 ) {
-        // No end given, infer duration if possible
-        // Otherwise, give end as MAX_VALUE
-        sub.end = toSeconds( node.getAttribute( "duration" ), 0 );
-
-        if ( sub.end >= 0 ) {
-          sub.end += sub.start;
-        } else {
-          sub.end = Number.MAX_VALUE;
-        }
-      }
-
-      return sub;
-    }
   });
 
+  // Get the "region" attribute of a node, to know where to put the subtitles
+  function getNodeRegion( node, defaultTo ) {
+    var region = node.getAttribute( "region" );
+
+    if ( region !== null ) {
+      return region;
+    } else {
+      return defaultTo || "";
+    }
+  }
+
+  // Parse a node for text content
+  function parseNode( node, timeOffset, region ) {
+    var sub = {},
+        track = {};
+
+    // Trim left and right whitespace from text and convert non-explicit line breaks
+    sub.text = node.textContent.replace( rWhitespace, "" ).replace( rLineBreak, "<br />" );
+    sub.id = node.getAttribute( "xml:id" ) || node.getAttribute( "id" );
+    sub.start = toSeconds ( node.getAttribute( "begin" ), timeOffset );
+    sub.end = toSeconds( node.getAttribute( "end" ), timeOffset );
+    sub.target = getNodeRegion( node, region);
+
+    if ( sub.end < 0 ) {
+      // No end given, infer duration if possible
+      // Otherwise, give end as MAX_VALUE
+      sub.end = toSeconds( node.getAttribute( "duration" ), 0 );
+
+      if ( sub.end >= 0 ) {
+        sub.end += sub.start;
+      } else {
+        sub.end = Number.MAX_VALUE;
+      }
+    }
+
+    track[ "subtitle" ] = sub;
+    return track;
+  }
 
   // Convert time expression to SS.mmm
   // Expression may be absolute to timeline (hh:mm:ss.ms)
@@ -135,7 +143,7 @@
       return parseFloat( t_in.substr( 0, i ) ) * getMultipler( t_in.substr( i ) ) + ( offset || 0 );
     }
   }
-  
+
   // In a time string such as 3.4ms, get the index of the first character (m) of the time metric (ms)
   function getMetricIndex( t_in ) {
     var i = t_in.length - 1;
@@ -155,12 +163,5 @@
       "s" : 1,
       "ms" : 0.001
     }[ metric ] || -1;
-  }
-
-  // Creates an object of all attributes keyed by name
-  function createTrack( name, attributes ) {
-    var track = {};
-    track[ name ] = attributes;
-    return track;
   }
 })( Popcorn );
--- parsers/parserTTML/popcorn.parserTTML.unit.js
+++ parsers/parserTTML/popcorn.parserTTML.unit.js
@@ -7,65 +7,75 @@ test( "Popcorn 1.0 TTML Parser Plugin", function () {
         {
           id: "subtitle1",
           text: "It seems a paradox, does it not,",
+          target: "subtitleArea",
           start: 0.76,
           end: 3.45
         },
         {
           id: "subtitle2",
           text: "that the image formed on<br />the Retina should be inverted?",
+          target: "subtitleArea",
           start: 5,
           end: 10
         },
         {
           id: "subtitle3",
           text: "It is puzzling, why is it<br />we do not see things upside-down?",
+          target: "subtitleArea",
           start: 10,
           end: 16
         },
         {
           id: "subtitle4",
           text: "You have never heard the Theory,<br />then, that the Brain also is inverted?",
+          target: "customRegion",
           start: 17.2,
           end: 23
         },
         {
           id: "subtitle5",
           text: "No indeed! What a beautiful fact!",
+          target: "subtitleArea",
           start: 23,
           end: 27
         },
         {
           id: "subtitle6",
           text: "It seems a paradox, does it not,",
+          target: "subtitle-container",
           start: 27.76,
           end: 30.45
         },
         {
           id: "subtitle7",
           text: "that the image formed on<br />the Retina should be inverted?",
+          target: "subtitle-container",
           start: 33,
           end: 37
         },
         {
           id: "subtitle8",
           text: "It is puzzling, why is it<br />we do not see things upside-down?",
+          target: "subtitle-container",
           start: 37,
           end: 43
         },
         {
           id: "subtitle9",
           text: "You have never heard the Theory,<br />then, that the Brain also is inverted?",
+          target: "subtitle-container",
           start: 44.2,
           end: 50
         },
         {
           id: "subtitle10",
           text: "No indeed! What a beautiful fact!",
+          target: "subtitle-container",
           start: 50,
           end: 54
         }
       ],
-      expects = subs.length * 4 + 1;
+      expects = subs.length * 5 + 1;
 
   function plus() {
     if ( ++count === expects ) {
@@ -89,6 +99,8 @@ test( "Popcorn 1.0 TTML Parser Plugin", function () {
         strictEqual( evt.start, sub.start, "Correctly parsed start of " + evt.id + " at " + evt.start );
         plus();
         strictEqual( evt.text, sub.text, "Correctly parsed text of " + evt.id + " at " + evt.start );
+        plus();
+        strictEqual( evt.target, sub.target, "Correctly parsed target of " + evt.id + " at " + evt.start );
         plus();
         strictEqual( evt.end, sub.end, "Correctly parsed end of " + evt.id + " at " + evt.start );
         plus();
