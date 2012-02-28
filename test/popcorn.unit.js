ad46b6813562a6f911639e439cbccd12561d03bd
ScottDowne <sdowne@learn.senecac.on.ca>
2012-02-28 16:31:21 -0500

    [#918] frame animation update tests fail without focus, with auseful message

--- test/popcorn.unit.js
+++ test/popcorn.unit.js
@@ -2086,6 +2086,12 @@ test( "Update Timer (frameAnimation)", function() {
 
   QUnit.reset();
 
+  if ( !document.hasFocus() ) {
+
+    ok( false, "frame animation tests need focus" );
+    return;
+  }
+
   var p2 = Popcorn( "#video", {
         frameAnimation: true
       }),
