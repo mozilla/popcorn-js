5ce1c5f04de53d1c4166260690c47911ab3db549
stevenaw <sweerdenburg@gmail.com>
2012-06-07 20:06:40 -0400

    [#752] Fixed linting issues. Simplified as well

--- parsers/parserTTML/popcorn.parserTTML.js
+++ parsers/parserTTML/popcorn.parserTTML.js
@@ -102,15 +102,14 @@
 
   // Parse a node for text content
   function parseNode( node, timeOffset, region ) {
-    var sub = {},
-        track = {};
+    var sub = {};
 
     // Trim left and right whitespace from text and convert non-explicit line breaks
     sub.text = node.textContent.replace( rWhitespace, "" ).replace( rLineBreak, "<br />" );
     sub.id = node.getAttribute( "xml:id" ) || node.getAttribute( "id" );
     sub.start = toSeconds ( node.getAttribute( "begin" ), timeOffset );
     sub.end = toSeconds( node.getAttribute( "end" ), timeOffset );
-    sub.target = getNodeRegion( node, region);
+    sub.target = getNodeRegion( node, region );
 
     if ( sub.end < 0 ) {
       // No end given, infer duration if possible
@@ -124,8 +123,7 @@
       }
     }
 
-    track[ "subtitle" ] = sub;
-    return track;
+    return { subtitle : sub };
   }
 
   // Convert time expression to SS.mmm
