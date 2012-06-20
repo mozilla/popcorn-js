c74f408e8bf80ae84b6c171808f26e54c2594234
Matthew Schranz <schranz.m@gmail.com>
2012-06-20 11:36:28 -0400

    [#1158] Added youtube player tests for ticket 1155

--- players/youtube/popcorn.youtube.unit.js
+++ players/youtube/popcorn.youtube.unit.js
@@ -440,13 +440,14 @@ asyncTest( "Popcorn YouTube Plugin Url Regex Test", function() {
 
 asyncTest( "Player height and width", function() {
 
-  expect( 4 );
+  expect( 6 );
 
   var popcorn1 = Popcorn.youtube( "#video4", "http://www.youtube.com/watch?v=nfGV32RNkhw" ),
       popcorn2 = Popcorn.youtube( "#video5", "http://www.youtube.com/watch?v=nfGV32RNkhw" ),
+      popcorn3 = Popcorn.youtube( "#customHeight", "http://www.youtube.com/watch?v=nfGV32RNkhw" ),
       readyStatePoll = function() {
 
-        if ( popcorn1.media.readyState !== 4 || popcorn2.media.readyState !== 4 ) {
+        if ( popcorn1.media.readyState !== 4 || popcorn2.media.readyState !== 4 || popcorn3.media.readyState !== 4 ) {
 
           setTimeout( readyStatePoll, 10 );
         } else {
@@ -456,16 +457,21 @@ asyncTest( "Player height and width", function() {
 
           equal( popcorn2.media.children[ 0 ].getAttribute( "width" ), 640, "Youtube player min width is 640" );
           equal( popcorn2.media.children[ 0 ].getAttribute( "height" ), 390, "Youtube player min height is 390" );
+          
+          equal( popcorn3.media.children[ 0 ].width, 800, "Youtube correctly gets width style of container (800)" );
+          equal( popcorn3.media.children[ 0 ].height, 450, "Youtube correctly gets height style of container (450)" );
 
           popcorn1.destroy();
           popcorn2.destroy();
+          popcorn3.destroy();
           start();
         }
       };
 
   popcorn1.volume( 0 );
   popcorn2.volume( 0 );
-
+  popcorn3.volume( 0 );
+  
   readyStatePoll();
 });
 
