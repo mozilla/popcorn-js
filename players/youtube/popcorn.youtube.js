72ee5ef486dad08061cd47b2c82b9ffc0bce2740
scottdowne <scott.downe@gmail.com>
2012-08-09 16:01:34 -0400

    [#1254] depricated warning on old youtube player, whitespace fixes, and timeout hack to allow for error events.

--- players/youtube/popcorn.youtube.js
+++ players/youtube/popcorn.youtube.js
@@ -2,15 +2,24 @@
 
   Popcorn.player( "youtube", {
     _canPlayType: function( nodeName, url ) {
-
-      return typeof url === "string" && Popcorn.HTMLYouTubeVideoElement.canPlayType( url ) && nodeName.toLowerCase() !== "video";
+      return ( typeof url === "string" &&
+               Popcorn.HTMLYouTubeVideoElement.canPlayType( url ) &&
+               nodeName.toLowerCase() !== "video" );
     }
   });
 
   Popcorn.youtube = function( container, url, options ) {
+    if ( typeof console !== "undefined" && console.warn ) {
+      console.warn( "Deprecated player 'youtube'. Please use Popcorn.HTMLYouTubeVideoElement directly." );
+    }
 
-    var media = Popcorn.HTMLYouTubeVideoElement( container );
-    media.src = url;
-    return Popcorn( media, options );
+    var media = Popcorn.HTMLYouTubeVideoElement( container ),
+        popcorn = Popcorn( media, options );
+
+    // Set the src "soon" but return popcorn instance first, so
+    // the caller can get get error events.
+    setTimeout( function() { media.src = url; }, 0 );
+
+    return popcorn;
   };
 }( window, Popcorn ));
