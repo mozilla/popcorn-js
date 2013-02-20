d742ee4c15c50e414b51946582e6aa3bd498582b
Matthew Schranz <schranz.m@gmail.com>
2013-02-20 12:43:14 -0500

    [t1443] Add setter method for duration on NullVideo

--- wrappers/null/popcorn.HTMLNullVideoElement.unit.js
+++ wrappers/null/popcorn.HTMLNullVideoElement.unit.js
@@ -6,6 +6,26 @@ var testData = {
 
   createMedia: function( id ) {
     return Popcorn.HTMLNullVideoElement( id );
+  },
+
+  playerSpecificAsyncTests: function() {
+
+    asyncTest( "NullVideo 01 - durationchange fired after setting duration", 1, function() {
+
+      var video = testData.createMedia( "#video" );
+
+      video.src = testData.videoSrc;
+
+      video.addEventListener( "durationchange", function onDurationChange() {
+        video.removeEventListener( "durationchange", onDurationChange, false );
+
+        equal( video.duration, 35, "Null video properly had it's new duration set." );
+        start();
+      }, false );
+
+      video.duration = 35;
+    });
+
   }
 
 };
