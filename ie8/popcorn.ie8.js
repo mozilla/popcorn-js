256420f8fa5191156dd9ec801022f3d1748e3c87
Lawrence Hunt <lawrence.hunt@gmail.com>
2013-04-05 22:52:30 +0100

    Updated formatting
    Replace single quotes with double quotes

--- ie8/popcorn.ie8.js
+++ ie8/popcorn.ie8.js
@@ -193,7 +193,7 @@
   if ( typeof String.prototype.trim !== "function" ) {
 
     String.prototype.trim = function() {
-      return this.replace(/^\s+|\s+$/g, '');
+      return this.replace(/^\s+|\s+$/g, "");
     };
   }
   
