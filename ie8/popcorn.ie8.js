41ef1e015d4b72ac6bb7407328c9b5be82bbbf6a
Lawrence Hunt <Lawrence.Hunt@Gamesys.co.uk>
2013-04-05 17:38:46 +0100

    Formatting fixes

--- ie8/popcorn.ie8.js
+++ ie8/popcorn.ie8.js
@@ -190,12 +190,10 @@
     }
   }
 
-  if( typeof String.prototype.trim !== 'function' ) {
+  if ( typeof String.prototype.trim !== "function" ) {
 
     String.prototype.trim = function() {
-
       return this.replace(/^\s+|\s+$/g, '');
-
     };
   }
   
